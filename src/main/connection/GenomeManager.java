91960d3aa52ab9123b633bdd72ff339d806d655b
Nicolas Toussaint <n.toussaint@etu.unistra.fr>
2015-03-08 17:34:58 +0100

    get the species lists

--- src/main/connection/GenomeManager.java
+++ src/main/connection/GenomeManager.java
@@ -2,11 +2,21 @@ package main.connection;
 
 import java.util.EventListener;
 import java.io.File;
-import main.util.Log;
-import java.io.IOException;
-import org.json.JSONObject;
 import java.io.PrintWriter;
+import java.util.Map;
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+import main.util.Log;
+import org.json.JSONObject;
+
 
 /**
  * Manages the species
@@ -15,9 +25,12 @@ import java.util.HashMap;
  */
 public class GenomeManager {
 	private File root_path;
+	private URL url;
+	private static final int BUFFER_SIZE = 4096;
 	
-	public GenomeManager (File root_path) {
+	public GenomeManager (File root_path, URL url) {
         this.root_path = root_path;
+        this.url = url;
         
         //If it's the first run, we initialize it
 		if(!this.root_path.exists()) {
@@ -40,7 +53,49 @@ public class GenomeManager {
     }
     
     public void AddSpeciesThreads(){//ExecutorService pool) {
-        
-
+        try{
+            JSONObject newSpecies = getSpecies();
+        } catch(IOException e) {
+            Log.e(e);
+        }
     }
+    
+    private JSONObject getSpecies()
+            throws IOException {
+        JSONObject JSONspecies = new JSONObject();
+        Map species = new HashMap();
+        
+        HttpURLConnection httpConn = (HttpURLConnection) this.url.openConnection();
+        int responseCode = httpConn.getResponseCode();
+ 
+        // always check HTTP response code first
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            String fileName = "";
+            String disposition = httpConn.getHeaderField("Content-Disposition");
+            String contentType = httpConn.getContentType();
+            int contentLength = httpConn.getContentLength();
+
+            System.out.println("Content-Type = " + contentType);
+            System.out.println("Content-Disposition = " + disposition);
+            System.out.println("Content-Length = " + contentLength);
+
+            // opens input stream from the HTTP connection
+            InputStream inputStream = httpConn.getInputStream();
+ 
+            int bytesRead = -1;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                Log.i(new String(buffer, "UTF-8"));
+            }
+ 
+            inputStream.close();
+        } else {
+            Log.e("No file to download. Server replied HTTP code: " + responseCode);
+        }
+        httpConn.disconnect();
+        
+        JSONspecies.put("species", species);
+        return JSONspecies;
+    }
+    
 }
--- src/main/core/ExcelLent.java
+++ src/main/core/ExcelLent.java
@@ -1,10 +1,13 @@
 package main.core;
 
+import java.io.File;
+import java.net.URL;
+import java.io.FileInputStream;
+import java.lang.Exception;
+
 import main.util.Log;
 import main.connection.GenomeManager;
-
-
-import java.io.File;
+import org.json.JSONObject;
 
 
 public class ExcelLent {
@@ -15,18 +18,44 @@ public class ExcelLent {
 
 	public static void main(String[] args) {
 	    File project_root = new File(System.getProperty("user.dir"));
+	    File urls_path = new File(project_root.getParent(), "urls_lists.json");
 	    File tree_root = new File(project_root.getParent(), "tree");
         
+        JSONObject urls = getUrls(urls_path);
 
         Log.i("Initializing the three genome manager");
         
-        virusesManager = new GenomeManager(new File(tree_root, "Viruses"));
-        eukaryotesManager = new GenomeManager(new File(tree_root, "Eukaryotes"));
-        prokaryotesManager = new GenomeManager(new File(tree_root, "Prokaryote"));
+        try{
+            virusesManager = new GenomeManager(new File(tree_root, "Viruses"),
+                                                new URL(urls.get("Viruses").toString()));
+            eukaryotesManager = new GenomeManager(new File(tree_root, "Eukaryotes"),
+                                                new URL(urls.get("Eukaryotes").toString()));
+            prokaryotesManager = new GenomeManager(new File(tree_root, "Prokaryote"),
+                                                new URL(urls.get("Prokaryote").toString()));
+        }catch(Exception e) {
+            Log.e(e);
+        }
         
         Log.i("Getting the lists and checking what to update");
         virusesManager.AddSpeciesThreads();
         eukaryotesManager.AddSpeciesThreads();
         prokaryotesManager.AddSpeciesThreads();
+	}
+	
+	
+	private static JSONObject getUrls(File urls_path) {
+	    String urls = null;
+	    try{
+            FileInputStream fis = new FileInputStream(urls_path);
+            byte[] data = new byte[(int) urls_path.length()];
+            fis.read(data);
+            fis.close();
+
+            urls = new String(data, "UTF-8");
+        } catch(Exception e) {
+            Log.e(e);
+        }
+        
+        return (JSONObject) new JSONObject(urls).get("urls");
 	}
 }
