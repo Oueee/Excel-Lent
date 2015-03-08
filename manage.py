2f3a82d0ad47f903ae4ab949966dc92558ea1280
Nicolas Toussaint <n.toussaint@etu.unistra.fr>
2015-03-08 14:46:49 +0100

    without eclipse gestion and readme maj

---
+++ manage.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+### compile parts
+def make():
+    call(['javac -d bin -sourcepath src src/com/core/Excellent.java'], shell=True)
+    #call(['javac -d bin -sourcepath src -cp "lib/*" src/com/sample/DroolsTest.java'], shell=True)
+
+
+### configure parts
+def configure():
+    print('hello')
+
+### test & analysze parts
+def test():
+    print('hello')
+
+### doc parts
+def doc():
+    print('hello')
+
+### run parts
+def run():
+    call(['java com.core.Excellent'], shell=True)
+    #call(['java -cp "bin:lib/*:." com.sample.DroolsTest'], shell=True)
+
+
+### clean parts
+def clean():
+    print('hello')
+
+operations = {
+    'make' :        (make, '\t\tcompile the software'),
+    'configure':    (configure, '\tconfigure it to adapt it at the architecture'),
+    'test':         (test, '\t\trun the unit test and integration test'),
+    'doc':          (doc, '\t\tcreate the doc'),
+    'run':          (run, '\t\texecute the software'),
+    'clean':        (clean, '\t\tclean the project'),
+}
+
+
+if __name__ == "__main__":
+    opt = ['make', 'run']
+    
+    if len(sys.argv) > 1:
+        opt = sys.argv[1:]
+    
+    if 'help' in opt:
+        for cmd_name, (stage, help) in operations.items():
+            print(cmd_name + ': ' + help)
+    else:
+        for stage in opt:
+            if not stage in operations:
+                print('The operation ' + stage + ' doesn\'t exists')
+            else:
+                current_stage = operations[stage][0]
+                current_stage()
