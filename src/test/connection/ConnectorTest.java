9759aa7fbd8e8637e928bf03a04a5ece08c84b9f
Björn Roß <b_ross01@uni-muenster.de>
2015-04-22 15:22:35 +0200

    Added time measurement in ConnectorTest

--- src/test/connection/ConnectorTest.java
+++ src/test/connection/ConnectorTest.java
@@ -8,9 +8,12 @@ public class ConnectorTest {
 
 	@Test
 	public void testConnector() throws IOException {
-
+		long startTime = System.nanoTime();
 		Connector c = new Connector();
-		c.downloadAndAnalyseReplicon("NC_021245.1");
-
+		//c.downloadAndAnalyseReplicon("NC_021245.1");
+		c.downloadAndAnalyseReplicon("NC_000001.11");
+		
+		long duration = (System.nanoTime() - startTime) / 1000000000; 
+		System.out.println("Download and analysis done in " + duration + " seconds");
 	}
 }
