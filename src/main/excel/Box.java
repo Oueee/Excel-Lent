ae87cfcd49e5752c028fe0d438d394be7a2aa87a
Nicolas Toussaint <n.toussaint@etu.unistra.fr>
2015-05-05 12:45:52 +0200

    box modified

--- src/main/excel/Box.java
+++ src/main/excel/Box.java
@@ -6,26 +6,26 @@ import java.util.TreeMap;
 
 public class Box {
 
-  public Map<String, Integer> p1 = new TreeMap<String, Integer>();
-	public Map<String, Integer> p2 = new TreeMap<String, Integer>();
-	public Map<String, Integer> p3 = new TreeMap<String, Integer>();
+  public Map<String, Integer>[] p = new TreeMap<String, Integer>[]();
 
 	public int nCds = 0;
 	public int nCdsNot = 0;
   public int nbNucleotides = 0;
 
-  public Box(TreeMap<String,Integer> p1,
-             TreeMap<String,Integer> p2,
-             TreeMap<String,Integer> p3,
+  public Box(TreeMap<String,Integer>[] p,
              int noCdsTraitees,
            	 int noCdsNonTraitees,
              int nbNucleotides)
   {
-    this.p1 = p1;
-    this.p2 = p2;
-    this.p3 = p3;
+    this.p = p;
     this.nCds = nCds;
     this.nCdsNot = nCdsNot;
     this.nbNucleotides = nbNucleotides;
+  }
+
+  public void add(Box b) {
+    this.nCds += b.nCds;
+    this.CdsNot += b.nCdsNot;
+    this.nbNucleotides += b.nbNucleotides;
   }
 }
